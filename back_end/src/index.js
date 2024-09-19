361f86ec5b6d2784fa23d0764a690793444e0630
saneem-bk <bksaneem@gmail.com>
2024-09-19 14:28:42 +0530

    commit before deployment

--- back_end/src/index.js
+++ back_end/src/index.js
@@ -13,7 +13,7 @@ const app = express();
 console.log(process.env.PORT)
 const port = process.env.PORT;
 app.use(cors({
-    origin: 'https://movie-rating-and-review.onrender.com',
+    origin: 'https://rate-it-movie.netlify.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
